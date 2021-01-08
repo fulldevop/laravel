b7bb0a86f0cf76e10e6fa7a1abad61e70f0d122d
fulldevop <vyatkina_nastya@mail.ru>
2021-01-08 17:07:23 +0300

    Lesson 1: auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/cabinet') }}">Cabinet</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
