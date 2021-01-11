dbd1f412221087b8e2346c1ae3f18a7ddbc38f31
fulldevop <vyatkina_nastya@mail.ru>
2021-01-12 00:12:06 +0300

    Lesson 1: packages, yarn, npm, webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('build')// понимает, где находится,
+    // что не просто в public - нужно при дальнейшей установке например шрифтов и пр.
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
+    .version();// чтобы использовалась актуальная сборка, добавляет id к файлам css и js
